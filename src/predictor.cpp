057b2e8506d0a957d49b5fcb617a35e37996bab9
Lin Ziyuan <ziyuan.19@intl.zju.edu.cn>
2025-02-11 18:38:10 -0800

    Modified

--- src/predictor.cpp
+++ src/predictor.cpp
@@ -32,8 +32,8 @@ int verbose;
 //Tournament bits.
 int ghistoryBits_tournament = 16;
 int lhistoryBits = 14;
-int pcIndexBits = 10;
-int choicerBits = 15;
+int pcIndexBits = 12;
+int choicerBits = 14;
 
 //------------------------------------//
 //      Predictor Data Structures     //
